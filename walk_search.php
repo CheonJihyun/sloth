42063694076164c4603dde27dd2063b370153fb7
Cheon Jihyun <jhyun1134@gmail.com>
2017-12-18 01:14:05 +0900

    addfiles

---
+++ walk_search.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+	<?php
+	$trail = $_REQUEST['trail'];
+	$course = $_REQUEST['course'];
+	$difficulty = $_REQUEST['difficulty'];
+	$time = $_REQUEST['time'];
+	$distance = $_REQUEST['distance'];
+
+	
+	?>
+</body>
+</html>
